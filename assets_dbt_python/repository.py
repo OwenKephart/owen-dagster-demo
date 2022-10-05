3eb2218ce62e27f80d52b2f9fdbd5cfd0634f92d
Owen Kephart <owen@elementl.com>
2022-10-05 13:59:45 -0700

    start

--- assets_dbt_python/repository.py
+++ assets_dbt_python/repository.py
@@ -1,36 +1,9 @@
+import os
 import json
+
 from dagster import repository, load_assets_from_package_module, file_relative_path, with_resources
-from dagster_dbt import load_assets_from_dbt_manifest, dbt_cli_resource
-
-from assets_dbt_python.assets import forecasting, population
-
-DBT_PROJECT_DIR = file_relative_path(__file__, "../dbt_project")
-DBT_PROFILES_DIR = file_relative_path(__file__, "../dbt_project/config")
 
 
 @repository
 def my_repository():
-    all_assets = (
-        load_assets_from_package_module(
-            population,
-            group_name="population",
-        )
-        + load_assets_from_dbt_manifest(
-            json.load(open(DBT_PROJECT_DIR + "/target/manifest.json")),
-        )
-        + load_assets_from_package_module(
-            forecasting,
-            group_name="forecasting",
-        )
-    )
-
-    return [
-        with_resources(
-            all_assets,
-            resource_defs={
-                "dbt": dbt_cli_resource.configured(
-                    {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
-                )
-            },
-        )
-    ]
+    return []
