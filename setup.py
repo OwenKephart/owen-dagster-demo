b140ca91f9b2c6115287c3269653eb8d1c4028e9
Owen Kephart <owen@elementl.com>
2022-10-06 07:38:50 -0700

    lxml

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
             "pandas",
             "numpy",
             "scipy",
+            "lxml",
             "dbt-core",
             "dbt-snowflake",
         ],
